0cb0b13b094a5778eacb76cc4996dba6db6ca462
kapupa <kurkeu@gmail.com>
2023-04-26 04:21:53 +0200

    Add project files.

---
+++ PudelkoLib/Pudelko.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace PudelkoLib
+{
+    public sealed class Pudelko
+    {
+        private readonly double a;
+        private readonly double b;
+        private readonly double c;
+        private readonly UnitOfMeasureType UnitOfMeasure;
+
+        public Pudelko(double a, double b, double c, UnitOfMeasureType unitOfMeasure)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+            UnitOfMeasure = unitOfMeasure;
+        }
+        //implementacja getterow
+        public double A
+        { get { return a; } }
+        public double B
+        { get { return b; } }
+        public double C
+        { get { return c; } }
+        public UnitOfMeasureType UnitOfMeasureType
+        { get { return UnitOfMeasure; } }
+        //ToString()
+        public override string ToString()
+        {
+            double aConverted = 0, bConverted = 0, cConverted = 0;
+            if (UnitOfMeasure == UnitOfMeasureType.milimeters)
+            {
+                aConverted = a / 1000;
+                bConverted = b / 1000;
+                cConverted = c / 1000;
+            }
+            else if (UnitOfMeasure == UnitOfMeasureType.centimeters)
+            {
+                aConverted = a / 100;
+                bConverted = b / 100;
+                cConverted = c / 100;
+            }
+            else if (UnitOfMeasure == UnitOfMeasureType.meters)
+            {
+                aConverted = a;
+                bConverted = b;
+                cConverted = c;
+            }
+            return $"{aConverted}m x {bConverted}m x {cConverted}m";
+        }
+
+    }
+}
---
+++ PudelkoLib/PudelkoEnums.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PudelkoLib
+{
+    public enum UnitOfMeasureType
+    {
+        milimeters,
+        centimeters,
+        meters
+    }
+}
