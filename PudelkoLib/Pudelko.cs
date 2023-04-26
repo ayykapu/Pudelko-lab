04280ba7465ea3d1b14d0bdf11344be78392ca13
kapupa <kurkeu@gmail.com>
2023-04-26 06:46:02 +0200

    fix

--- PudelkoLib/Pudelko.cs
+++ PudelkoLib/Pudelko.cs
@@ -2,14 +2,29 @@
 
 namespace PudelkoLib
 {
-    public class Pudelko
+    public sealed class Pudelko
     {
-                aConverted = a;
-                bConverted = b;
-                cConverted = c;
-            }
-            return $"{aConverted}m x {bConverted}m x {cConverted}m";
-        }
+        private readonly double a;
+        private readonly double b;
+        private readonly double c;
+        private readonly UnitOfMeasureType unitOfMeasure;
 
+        public Pudelko(double a, double b, double c, UnitOfMeasureType unitOfMeasure)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+            this.unitOfMeasure = unitOfMeasure;
+        }
+        public double A
+        { get { return a; } }
+        public double B
+        { get { return b; } }
+        public double C
+        { get { return c; } }
+        public UnitOfMeasureType UnitOfMeasure
+        { get { return unitOfMeasure; } }
+            
+       
     }
 }
